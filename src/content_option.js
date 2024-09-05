da2112d831b97d8b27821d2945987ef6649563b6
Carlos Palacios <carlos_a.riascos@uao.edu.co>
2024-09-05 17:26:04 -0500

    set portfolio

--- src/content_option.js
+++ src/content_option.js
@@ -1,144 +1,98 @@
-const logotext = "JOHN";
+const logotext = "Carlos Palacios";
 const meta = {
-    title: "Carlos Palacios",
+    title: "Carlos Alberto Riascos Palacios",
     description: "I’m a multimedia engineer and game developer _ I always develop with love and trying to do my best",
 };
 
 const introdata = {
-    title: "I’m Carlos Palacios",
+    title: "I’m Carlos Alberto Riascos Palacios",
     animated: {
         first: "I love coding",
         second: "I love playing videogames",
         third: "I develop videogames",
     },
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
-    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+    description: "As a dedicated game developer, I am passionate about creating exceptional gaming experiences that meet the highest standards of quality.",
+    your_img_url: "https://scontent.fclo8-1.fna.fbcdn.net/v/t39.30808-6/453152626_26081917481422954_735986331124208644_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeHZE1X8a3sbJWV-YaFtldjjNLjHs-j0VlQ0uMez6PRWVNydci1TsGqW_lBkhMBb272j9k9ubHUq_8Rc87hnsAyn&_nc_ohc=qodhlxWe91AQ7kNvgG1siyk&_nc_ht=scontent.fclo8-1.fna&oh=00_AYAl8ntYhqoZnseQITL1nNsJRcIxVxPbM56kZ-cySQKWjg&oe=66DFD369",
 };
 
 const dataabout = {
-    title: "abit about my self",
-    aboutme: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
+    title: "A bit about my self",
+    aboutme: "I am a passionate video game developer with extensive experience in Unity and a strong foundation in Multimedia Engineering. I love creating immersive and engaging gaming experiences, with a focus on designing game mechanics, user interfaces, and optimizing performance. I'm always eager to learn and adapt to new technologies.",
 };
 const worktimeline = [{
-        jobtitle: "Designer of week",
-        where: "YAdfi",
-        date: "2020",
+        jobtitle: "Unity Developer",
+        where: "Universal Phoenix Group",
+        date: "2023-2024",
     },
     {
-        jobtitle: "Designer of week",
-        where: "Jamalya",
-        date: "2019",
-    },
-    {
-        jobtitle: "Designer of week",
-        where: "ALquds",
-        date: "2019",
+        jobtitle: "Unity Developer",
+        where: "Generation",
+        date: "2024",
     },
 ];
 
 const skills = [{
-        name: "Python",
+        name: "Unity",
+        value: 95,
+    },
+    {
+        name: "C#",
+        value: 95,
+    },
+    {
+        name: "Git",
         value: 90,
     },
     {
-        name: "Djano",
-        value: 85,
+        name: "Visual Studio",
+        value: 92,
     },
     {
-        name: "Javascript",
-        value: 80,
-    },
-    {
-        name: "React",
-        value: 60,
-    },
-    {
-        name: "Jquery",
-        value: 85,
+        name: "JavaScript",
+        value: 70,
     },
 ];
 
 const services = [{
-        title: "UI & UX Design",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+        title: "Gameplay Programmer",
+        description: "As a gameplay programmer, I specialize in developing and implementing core game mechanics that enhance player interaction and immersion. My expertise lies in creating smooth, responsive gameplay systems, optimizing performance, and solving complex technical challenges. I focus on ensuring that the game experience is both engaging and highly polished, bringing innovative ideas to life with precision and creativity.",
     },
     {
-        title: "Mobile Apps",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
-    },
-    {
-        title: "Wordpress Design",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+        title: "UI Developer",
+        description: "As a UI developer, I focus on implementing user interfaces that enhance the player's experience. I take meticulously designed UI elements and integrate them seamlessly into the game, ensuring functionality, responsiveness, and visual consistency. My goal is to create intuitive, user-friendly interfaces that elevate gameplay while maintaining smooth performance and attention to detail.",
     },
 ];
 
 const dataportfolio = [{
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://cdn.discordapp.com/attachments/852766959711944714/1281355302423367760/BrokenDimension_1_1_1.png?ex=66db6a9e&is=66da191e&hm=0b558459b72def97c2acf77d1914c4526176c30b0718a9a8443559d37acd5582&",
+        description: "In Broken Dimension: Dual Crystal, you navigate between two contrasting dimensions—a peaceful Light Dimension and a perilous Darkness Dimension.",
+        link: "https://changuacq.itch.io/broken-dimension-dual-crystal",
     },
     {
-        img: "https://picsum.photos/400/800/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://cdn.discordapp.com/attachments/852766959711944714/1281358589042233537/1YEPo5.png?ex=66db6dae&is=66da1c2e&hm=247f33f73eacef3a2364d0fe4ee601627ff96c474bd456d861ec14671f38e2db&",
+        description: "Ghost Whisperer: Anne's Diary is an interactive narrative game that immerses you in the intriguing and supernatural life of Anne, a young woman with the extraordinary ability to communicate with ghosts.",
+        link: "https://acchan23.itch.io/ghost-whisperer-annes-diary",
     },
     {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://cdn.discordapp.com/attachments/852766959711944714/1281366865616179282/imagen_2024-09-05_163359885.png?ex=66db7563&is=66da23e3&hm=d057f9d57796a17bdeeb5f5b7c2c8f750201e1a98ee43ad691000eb416bd4798&",
+        description: "A video game that allows you to have an immersive multimedia experience, enabling you to take on the role of a vehicle driver while following traffic rules in the city of Cali, Colombia",
+        link: "https://carlosryder.itch.io/autodidactico",
+    },    {
+        img: "https://cdn.discordapp.com/attachments/852766959711944714/1281364214749069493/ZkNKZL_2.png?ex=66db72eb&is=66da216b&hm=a600b15ff49a37140b7838e8eb39221d6de7ea35d2e21e9c3c45ac05e86ccdd5&",
+        description: "Experience the thrill of driving in CarDriver Simulator - Pizza Lover! The objective is simple: collect every pizza on your way to the finish line without colliding with any obstacles and before the time is out.",
+        link: "https://carlosryder.itch.io/cardriver-simulator-pizza-lover",
     },
     {
-        img: "https://picsum.photos/400/600/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/300/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/700/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-
-    {
-        img: "https://picsum.photos/400/600/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/300/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/550/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/700/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://cdn.discordapp.com/attachments/852766959711944714/1281362391271215156/the_worst_assasin.png?ex=66db7138&is=66da1fb8&hm=65b9a49c682fe70f8967005e88533c81c99c944aa9247f96fb79ddd3f7177270&",
+        description: "Experience a new game of platforming in The Worst Assassin! The objective is simple: navigate through the challenging level and defeat your enemies. But be careful, every time you attack, you hurt yourself. Master the art of evasion and strategy to survive and complete your mission before it's too late.",
+        link: "https://sofiarizap.itch.io/the-worst-assassin",
     },
 ];
 
 const contactConfig = {
-    YOUR_EMAIL: "name@domain.com",
-    YOUR_FONE: "(555)123-4567",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    YOUR_EMAIL: "carlospalacios78@hotmail.com",
+    /*YOUR_FONE: "(57) 320-484-1937",*/
+    description: "Feel free to contact me for business inquiries. I'm always open to hearing new proposals and projects. Let's collaborate and bring innovative ideas to life!",
     // creat an emailjs.com account 
     // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
     YOUR_SERVICE_ID: "service_id",
@@ -147,10 +101,8 @@ const contactConfig = {
 };
 
 const socialprofils = {
-    github: "https://github.com",
-    facebook: "https://facebook.com",
-    linkedin: "https://linkedin.com",
-    twitter: "https://twitter.com",
+    github: "https://github.com/CarlosRyder",
+    linkedin: "https://www.linkedin.com/in/carlos-palacios-unity-developer/",
 };
 export {
     meta,
